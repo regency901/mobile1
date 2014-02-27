3716bbc54ef88b01d51a6ca3abddf95b21a2173d
Michel <regency901@gmail.com>
2014-02-26 21:39:12 -0500

    Added handlebar template

--- js/main.js
+++ js/main.js
@@ -24,9 +24,8 @@ var app = {
     initialize: function() {
         var self = this;
         this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 };
 
