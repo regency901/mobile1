fdd00432d33d64a9be850d12bd57423c31870d22
Michel <regency901@gmail.com>
2014-03-01 10:56:42 -0500

    Part 10 - location

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -9,7 +9,23 @@ var EmployeeView = function(employee) {
         return this;
     };
 
-    this.initialize();
+    this.addLocation = function(event) {
+        event.preventDefault();
+        console.log('addLocation');
+        navigator.geolocation.getCurrentPosition(
+            function(position) {
+                $('.location', this.el).html(position.coords.latitude + ',' + position.coords.longitude);
+            },
+            function() {
+                alert('Error getting location');
+            });
+        return false;
+    };
+
+    this.initialize() {
+
+    this.el.on('click', '.add-location-btn', this.addLocation);
+    };
 
  
 }
