cb74a7cf5fc37503c3912d88cb8601e018ae7eae
Michel <regency901@gmail.com>
2014-03-02 19:43:36 -0500

    Part 12

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -4,6 +4,7 @@ var EmployeeView = function(employee) {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
         this.el.on('click', '.add-contact-btn', this.addToContacts);
+        this.el.on('click', '.change-pic-btn', this.changePicture);
     };
  
     this.render = function() {
@@ -23,14 +24,12 @@ var EmployeeView = function(employee) {
         event.preventDefault();
         console.log('addToContacts');
 
-        app.showAlert("About to add a contact ..."+navigator.contacts, "Debug");
+        if (!navigator.contacts) {
+            app.showAlert("Contacts API not supported", "Error");
+            return;
+        }
 
-//        if (!navigator.contacts) {
-//            app.showAlert("Contacts API not supported", "Error");
-//            return;
-//        }
         var contact = navigator.contacts.create();
-        app.showAlert("contacts.create() called", "Debug");
         contact.name = {givenName: employee.firstName, familyName: employee.lastName};
         var phoneNumbers = [];
         phoneNumbers[0] = new ContactField('work', employee.officePhone, false);
@@ -50,6 +49,30 @@ var EmployeeView = function(employee) {
             function() {
                 alert('Error getting location');
             });
+        return false;
+    };
+
+    this.changePicture = function(event) {
+        event.preventDefault();
+        if (!navigator.camera) {
+            app.showAlert("Camera API not supported", "Error");
+            return;
+        }
+        var options =   {   quality: 50,
+                            destinationType: Camera.DestinationType.DATA_URL,
+                            sourceType: 1,      // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+                            encodingType: 0     // 0=JPG 1=PNG
+                        };
+     
+        navigator.camera.getPicture(
+            function(imageData) {
+                $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+            },
+            function() {
+                app.showAlert('Error taking picture', 'Error');
+            },
+            options);
+     
         return false;
     };
 
