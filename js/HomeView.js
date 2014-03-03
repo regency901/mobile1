69b17e9f24783a36215d7bbbdbdc6026af000c72
Michel <regency901@gmail.com>
2014-03-02 20:40:41 -0500

    Part 13 homePage fix

--- js/HomeView.js
+++ js/HomeView.js
@@ -2,7 +2,7 @@ var HomeView = function(store) {
  
     this.initialize = function() {
         // Define a div wrapper for the view. The div wrapper is used to attach events.
-        this.el = $('<div/>');
+        this.el = $('<div/>').attr("id", "homePage");
         this.el.on('keyup', '.search-key', this.findByName);
     };
 
